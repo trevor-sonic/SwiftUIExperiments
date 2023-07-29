88d5875b2b8c1c5955dc51c5afd097518d79a413
(Trevor) Duygun Beydag <Duygun.Beydag@proagrica.com>
2023-07-29 01:23:11 +0100

    Array Type is added.

---
+++ Shared/RecursiveCD/ArrayTypesList.swift
@@ -0,0 +1,18 @@
+//
+//  ArrayTypesList.swift
+//  Experimental
+//
+//  Created by Beydag, (Trevor) Duygun (Proagrica-HBE) on 28/07/2023.
+//
+
+import SwiftUI
+
+struct ArrayTypesList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArrayTypesList()
+}
